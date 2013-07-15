40d11542cead8ca80f6d1f697c0e055e07096a31
Amit <amitvarsani121@googlemail.com>
2013-07-15 16:54:53 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -19,3 +19,11 @@ public int[] makePi() {
   int[] pi = {3,1,4};
   return pi;
 }
+
+/////// commonEnd ////////////////////////////////////////////////////////////////
+public boolean commonEnd(int[] a, int[] b) {
+  if(a[0] == b[0] || a[a.length-1] == b[b.length-1])
+   return true;
+  else 
+   return false;
+}
