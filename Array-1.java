6af55149c9990f77f1db1226abb88ef47c57bba2
Amit <amitvarsani121@googlemail.com>
2013-07-19 15:17:07 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -62,3 +62,17 @@ public int[] reverse3(int[] nums) {
   nums[2]=rev[0];
   return nums;
 }
+
+/////// maxEnd3 ////////////////////////////////////////////////////////////////////
+public int[] maxEnd3(int[] nums) {
+  int max;
+  if(nums[0]>nums[2])
+   max=nums[0];
+  else
+   max=nums[2];
+
+  for(int i=0; i<nums.length; i++){
+   nums[i] = max;
+  }
+  return nums;
+}
