b310ffbc478dfa87d4fb3cdc6221c1d433593406
Amit <amitvarsani121@googlemail.com>
2013-07-23 23:21:05 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -108,3 +108,11 @@ public int[] makeEnds(int[] nums) {
   newArray[1] = nums[nums.length-1];
   return newArray;
 }
+
+/////// has23 ////////////////////////////////////////////////////////////////////////
+public boolean has23(int[] nums) {
+  if((nums[0] == 2 || nums[1] == 3) || (nums[0] == 3 || nums[1] == 2))
+   return true;
+  else 
+   return false;
+}
