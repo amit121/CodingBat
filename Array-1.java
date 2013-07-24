4d6f3f478b8688df671d6cada7b725ec341cfcf3
Amit <amitvarsani121@googlemail.com>
2013-07-24 20:34:07 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -116,3 +116,12 @@ public boolean has23(int[] nums) {
   else 
    return false;
 }
+
+/////// no23 /////////////////////////////////////////////////////////////////////////
+public boolean no23(int[] nums) {
+  if(nums[0] != 2 && nums[0] != 3){
+   if(nums[1] != 3 && nums[1] != 2)
+    return true;
+  }
+  return false;
+}
