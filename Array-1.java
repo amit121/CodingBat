7de9f67fb944dfd8535a418054e77c55792c2e3d
Amit <amitvarsani121@googlemail.com>
2013-07-26 23:08:08 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -132,3 +132,15 @@ public int[] makeLast(int[] nums) {
   doubleArray[doubleArray.length-1]=nums[nums.length-1];
   return doubleArray;
 }
+
+/////// double23 /////////////////////////////////////////////////////////////////////
+public boolean double23(int[] nums) {
+  if(nums.length == 0)
+   return false;
+  else if(nums.length == 1 && (nums[0] != 2 || nums[0] != 3))
+   return false;
+  else if((nums[0] == 2 && nums[1] == 2) ||(nums[0] == 3 && nums[1] == 3))
+   return true;
+  else
+   return false;
+}
