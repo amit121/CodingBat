18f6bc2d23371926dbc63b77ccdd06630c796e66
Amit <amitvarsani121@googlemail.com>
2013-07-27 20:12:48 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -144,3 +144,17 @@ public boolean double23(int[] nums) {
   else
    return false;
 }
+
+/////// fix23 /////////////////////////////////////////////////////////////////////////
+public int[] fix23(int[] nums) {
+  if(nums[0] == 2 && nums[1] ==3){
+   nums[1]=0;
+   return nums;
+  }
+  else if(nums[1] == 2 && nums[2] ==3){
+   nums[2]=0;
+   return nums;
+  }
+  else
+   return nums;
+}
