516d019c541d5229c0084e95cd0af0c32eb316b7
Amit <amitvarsani121@googlemail.com>
2013-08-05 00:31:10 +0100

    Update Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -261,3 +261,14 @@ public int[] frontPiece(int[] nums) {
   }
   return temp;
 }
+
+/////// unlucky1 //////////////////////////////////////////////////////////////////////////
+public boolean unlucky1(int[] nums) {
+
+  if(nums.length<2)
+   return false;
+
+  if(nums[0] == 1 && nums[1] == 3 || nums[1] == 1 && nums[2] == 3 ||nums[nums.length-2] == 1 && nums[nums.length-1] == 3)
+   return true;
+  return false;
+}
