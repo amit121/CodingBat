997e72e72f99b9fbc76709835cdd13b21fcd7fd8
Amit <amitvarsani121@googlemail.com>
2013-08-08 20:41:16 +0100

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -173,3 +173,13 @@ public boolean hasTeen(int a, int b, int c) {
   else
    return false;
 }
+
+/////// loneTeen /////////////////////////////////////////////////////////////////////
+public boolean loneTeen(int a, int b) {
+  boolean aTeen = (a>=13 && a<=19);
+  boolean bTeen = (b>=13 && b<=19);
+  if(aTeen && !bTeen || !aTeen && bTeen) 
+   return true;
+  else
+   return false;
+}
