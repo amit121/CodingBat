16bda90b8c87c48f78132c008c51a17e3afca650
Amit <amitvarsani121@googlemail.com>
2013-08-09 22:35:20 +0100

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -183,3 +183,13 @@ public boolean loneTeen(int a, int b) {
   else
    return false;
 }
+
+/////// delDel ////////////////////////////////////////////////////////////////////////
+public String delDel(String str) {
+  if(str.length()<4)
+   return str;
+  else if(str.substring(1,4).equals("del"))
+   return str.substring(0,1)+str.substring(4,str.length());
+  else
+   return str;
+}
