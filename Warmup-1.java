d9f70cad3316420efdcd25d8513bce64afb01267
Amit <amitvarsani121@googlemail.com>
2013-08-11 19:09:46 +0100

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -201,3 +201,15 @@ public boolean mixStart(String str) {
   else
    return false;
 }
+
+/////// startOz /////////////////////////////////////////////////////////////////////////
+public String startOz(String str) {
+  if(str.length()>=2 && str.substring(0,1).equals("o") && str.substring(1,2).equals("z"))
+   return "oz";
+  else if(str.length()>=1 && str.substring(0,1).equals("o"))
+   return "o";
+  else if(str.length()>=1 && str.substring(1,2).equals("z"))
+   return "z";
+  else
+   return "";
+}
