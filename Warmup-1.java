b6fe2fa67f9001f6ac45e7f251460344deab0ea7
Amit <amitvarsani121@googlemail.com>
2013-08-12 10:18:53 +0100

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -213,3 +213,15 @@ public String startOz(String str) {
   else
    return "";
 }
+
+/////// intMax ///////////////////////////////////////////////////////////////////////////
+public int intMax(int a, int b, int c) {
+  int max;
+  if(a>b)
+   max = a;
+  else
+   max = b;
+  if(c>max)
+   max = c;
+  return max;
+}
