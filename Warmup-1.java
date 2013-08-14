1362c46182110caa68120d4cf2784ee0f7cf1f32
Amit <amitvarsani121@googlemail.com>
2013-08-14 22:57:18 +0100

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -239,3 +239,17 @@ public int close10(int a, int b) {
   else
    return 0;
 }
+
+/////// in3050 /////////////////////////////////////////////////////////////////////////////
+public boolean in3050(int a, int b) {
+  boolean aRange1 = (a>=30 && a<=40);
+  boolean bRange1 = (b>=30 && b<=40);
+  boolean aRange2 = (a>=40 && a<=50);
+  boolean bRange2 = (b>=40 && b<=50);
+  
+  if(aRange1 && bRange1 || aRange2 && bRange2)
+   return true;
+  else
+   return false;
+  
+}
