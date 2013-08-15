0e48786c797f0396ee838cb24088a981aff683f8
Amit <amitvarsani121@googlemail.com>
2013-08-15 18:40:53 +0100

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -253,3 +253,18 @@ public boolean in3050(int a, int b) {
    return false;
   
 }
+
+/////// max1020 ////////////////////////////////////////////////////////////////////////////
+public int max1020(int a, int b) {
+  if (b>a){
+   int temp = a;
+   a = b;
+   b = temp;
+  }
+  if(a>= 10 && a <= 20) 
+   return a;
+  else if(b>= 10 && b <= 20) 
+   return b;
+  else
+   return 0;
+}
