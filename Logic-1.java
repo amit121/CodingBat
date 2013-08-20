23ee2cc3485a7d9760b123c9022716a5801fbeb8
Amit <amitvarsani121@googlemail.com>
2013-08-20 18:40:18 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -3,7 +3,7 @@ public boolean cigarParty(int cigars, boolean isWeekend) {
   if(isWeekend && cigars >= 40)
     return true;
   else if(cigars >= 40 && cigars <=60)
-   return true;
+    return true;
   else
-   return false;
+    return false;
 }
