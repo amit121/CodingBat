43acf516870b54998ff381499367970fe5b38fbb
Amit <amitvarsani121@googlemail.com>
2013-08-21 10:56:39 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -7,3 +7,13 @@ public boolean cigarParty(int cigars, boolean isWeekend) {
   else
     return false;
 }
+
+/////// dateFashion ////////////////////////////////////////////////////////////////
+public int dateFashion(int you, int date) {
+  if(you<= 2 || date <= 2)
+   return 0;
+  else if(you>= 8 || date >= 8)
+   return 2;
+  else
+   return 1;
+}
