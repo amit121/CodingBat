bdf3b0291e659a750a437c3a83e4eab05684f66e
Amit <amitvarsani121@googlemail.com>
2013-08-23 11:10:48 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -27,3 +27,22 @@ public boolean squirrelPlay(int temp, boolean isSummer) {
   else
    return false;
 }
+
+/////// caughtSpeeding ///////////////////////////////////////////////////////////////
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  if(isBirthday){
+   if(speed<=65)
+    return 0;
+   else if(speed>=66 && speed<=85)
+    return 1;
+   else
+    return 2;
+  }
+   
+  if(speed<=60)
+   return 0;
+  else if(speed>=61 && speed<=80)
+   return 1;
+  else
+   return 2;
+}
