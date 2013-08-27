ca71a96270ab19a51c7aee2df6df4a10356f02e7
Amit <amitvarsani121@googlemail.com>
2013-08-27 10:38:19 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -76,3 +76,17 @@ public boolean love6(int a, int b) {
   else
    return false;
 }
+
+/////// in1To10 /////////////////////////////////////////////////////////////////////////
+public boolean in1To10(int n, boolean outsideMode) {
+  if(outsideMode){
+   if(n<=1 || n>=10)
+    return true;
+   else
+    return false;
+  }
+  if(n>=1 && n<=10)
+   return true;
+  else
+   return false;
+}
