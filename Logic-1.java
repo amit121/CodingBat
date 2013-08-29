8f57bd4ca29ff94b61b5f068d80ea3a1d811ab36
Amit <amitvarsani121@googlemail.com>
2013-08-29 19:09:07 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -98,3 +98,11 @@ public boolean specialEleven(int n) {
   else 
    return false;
 }
+
+/////// more20 ///////////////////////////////////////////////////////////////////////////
+public boolean more20(int n) {
+  if(n%20==1 || n%20==2)
+   return true;
+  else
+   return false;
+}
