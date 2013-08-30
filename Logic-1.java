02f4e1f9242131df0672fffa5b808267859a14b9
Amit <amitvarsani121@googlemail.com>
2013-08-30 19:04:04 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -106,3 +106,13 @@ public boolean more20(int n) {
   else
    return false;
 }
+
+/////// old35 /////////////////////////////////////////////////////////////////////////////
+public boolean old35(int n) {
+  if(n%3==0 && n%5==0)
+   return false;
+  else if(n%3==0 || n%5==0)
+   return true;
+  else
+   return false;
+}
