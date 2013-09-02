ffec80e635992144c13c42a7956cf15dde489b4b
Amit <amitvarsani121@googlemail.com>
2013-09-02 09:40:25 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -133,3 +133,11 @@ public boolean nearTen(int num) {
   else
    return false;
 }
+
+/////// teenSum ///////////////////////////////////////////////////////////////////////////
+public int teenSum(int a, int b) {
+  if(a>=13 && a<=19 || b>=13 && b<=19)
+   return 19;
+  else
+   return a+b;
+}
