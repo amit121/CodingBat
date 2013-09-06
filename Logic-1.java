83f1f6f9c885118e6667438e9f3bb6f16a9b57ef
Amit <amitvarsani121@googlemail.com>
2013-09-06 11:08:57 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -175,3 +175,15 @@ public String fizzString(String str) {
   else
    return str;
 }
+
+/////// fizzString2 //////////////////////////////////////////////////////////////////////////
+public String fizzString2(int n) {
+  if(n%3==0 && n%5==0)
+   return "FizzBuzz!";
+  else if(n%3==0)
+   return "Fizz!";
+  else if(n%5==0)
+   return "Buzz!";
+  else
+   return n+"!";
+}
