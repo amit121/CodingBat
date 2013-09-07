55396103f22e812de8df625e09424705ced025f3
Amit <amitvarsani121@googlemail.com>
2013-09-07 17:38:42 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -187,3 +187,11 @@ public String fizzString2(int n) {
   else
    return n+"!";
 }
+
+/////// twoAsOne //////////////////////////////////////////////////////////////////////////////
+public boolean twoAsOne(int a, int b, int c) {
+  if(a+b==c || b+c==a || a+c==b)
+   return true;
+  else
+   return false;
+}
