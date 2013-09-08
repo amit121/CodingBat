df2e26f4c1f95503caf0b7ce765c7df1cc1518b0
Amit <amitvarsani121@googlemail.com>
2013-09-08 19:46:23 +0100

    Update Logic-1.java

--- Logic-1.java
+++ Logic-1.java
@@ -195,3 +195,13 @@ public boolean twoAsOne(int a, int b, int c) {
   else
    return false;
 }
+
+/////// inOrder ////////////////////////////////////////////////////////////////////////////////
+public boolean inOrder(int a, int b, int c, boolean bOk) {
+  if(bOk && c>b)
+   return true;
+  else if(!bOk && b>a && c>b)
+   return true;
+  else
+   return false;
+}
